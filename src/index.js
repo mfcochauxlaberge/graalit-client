8b3c476bd1c7f7edfeb432140b51280208f474cc
Marc-François Cochaux-Laberge <dev@mfcl.io>
2018-08-09 20:48:31 -0400

    Initialize repository

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+import './styles/main.css'
+
+const App = () => {
+  return <h1>Graalit</h1>
+}
+
+ReactDOM.render(<App />, document.getElementById('app'))
---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const HTMLWebpackPlugin = require("html-webpack-plugin")
+
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        }
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      }
+    ]
+  },
+  plugins: [
+    new HTMLWebpackPlugin({
+      template: './src/index.html',
+      filename: './index.html',
+    }),
+  ],
+}
