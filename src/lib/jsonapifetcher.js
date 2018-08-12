53bcf18b7b466017242caea0123dcf2672f5f0a0
Marc-François Cochaux-Laberge <dev@mfcl.io>
2018-08-11 23:57:38 -0400

    Attempt a basic API for JSONAPIFetcher

---
+++ src/lib/jsonapifetcher.js
@@ -0,0 +1,20 @@
+class JSONAPIFetcher {
+  base = ''
+
+  getOne = ({ type, id, fields, include }) => {
+  }
+
+  getMany = ({ type, fields, filter, sort, pageSize, pageNumber, include }) => {
+  }
+
+  post = ({ type, data, include }) => {
+  }
+
+  patch = ({ type, id, updates }) => {
+  }
+
+  delete = ({ type, id }) => {
+  }
+}
+
+export { JSONAPIFetcher }
