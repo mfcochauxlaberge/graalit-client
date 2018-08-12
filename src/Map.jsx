7f25df695b1c7f080ea6d2776e9c739650fe13e8
Marc-François Cochaux-Laberge <dev@mfcl.io>
2018-08-11 23:30:35 -0400

    Make an empty map

--- src/Map.jsx
+++ src/Map.jsx
@@ -1,6 +1,12 @@
 // React
 import React, { Component, Fragment } from 'react'
 
+// Libs
+import { times } from 'lodash'
+
+// Infrastructure
+import Empty from './assets/img/infrastructure/empty.png'
+
 class Map extends Component {
   constructor(props) {
     super(props)
@@ -10,7 +16,17 @@ class Map extends Component {
     return (
       <Fragment>
         <h2>Map</h2>
-        <p>No map yet.</p>
+        <div id="map">
+          {
+            times(16, (n) => {
+              return (
+                <div className="cell">
+                  <img src={Empty} alt="Empty cell" />
+                </div>
+              )
+            })
+          }
+        </div>
       </Fragment>
     )
   }
