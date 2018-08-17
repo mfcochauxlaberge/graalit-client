df86961b228075cd610cb63ef9ced9753e82bc08
Marc-François Cochaux-Laberge <dev@mfcl.io>
2018-08-16 21:48:06 -0400

    Move transform-class-properties from webpack config to .babelrc

--- webpack.config.js
+++ webpack.config.js
@@ -22,9 +22,6 @@ module.exports = {
         exclude: /node_modules/,
         use: {
           loader: 'babel-loader',
-          query: {
-            plugins: ['transform-class-properties'],
-          },
         },
       },
       {
