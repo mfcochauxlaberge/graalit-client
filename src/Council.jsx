1146b25cde96140842e8156a71c58bcac6d48192
Marc-François Cochaux-Laberge <dev@mfcl.io>
2018-08-25 23:25:18 -0400

    Start implementing mock Council page

--- src/Council.jsx
+++ src/Council.jsx
@@ -11,6 +11,37 @@ class Council extends Component {
       <Fragment>
         <h2>Council</h2>
         <p>No council yet.</p>
+        <h3>Orders</h3>
+        <button>Previous turn</button>
+        <h4>Turn 8 (1 day ago)</h4>
+        <span>
+          <i>No orders given.</i>
+        </span>
+        <h4>Turn 9 (12 hours ago)</h4>
+        <span>
+          <i>No orders given.</i>
+        </span>
+        <h4>Turn 10 (in 3 minutes)</h4>
+        <span>
+          <i>No orders given.</i>
+        </span>
+        <h4>Turn 10 (in 12 hours)</h4>
+        <span>
+          <i>No orders given.</i>
+        </span>
+        <h4>Turn 11 (in 1 day)</h4>
+        <span>
+          <i>No orders given.</i>
+        </span>
+        <h4>Turn 12 (in 1 day)</h4>
+        <span>
+          <i>No orders given.</i>
+        </span>
+        <h4>Turn 13 (in 2 days)</h4>
+        <span>
+          <i>No orders given.</i>
+        </span>
+        <button>Next turn</button>
       </Fragment>
     )
   }
